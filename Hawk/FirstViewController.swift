cdce1faaeb7d59ed52301b0081128f64cef7bdec
shreyashirday@gmail.com <shreyashirday@gmail.com>
2016-10-26 18:44:45 -0400

    Initial Commit

---
+++ Hawk/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Hawk
+//
+//  Created by Shreyas Hirday on 10/26/16.
+//  Copyright © 2016 HirDaysOfTheWeek. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
